1bc355e4a56102142a48871f2f1886ca1a99445b
Stephen Guillory <stepheng1492@gmail.com>
2019-06-26 11:39:22 -0500

    SOS sms functional

--- src/app/add-guardian.service.ts
+++ src/app/add-guardian.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { HttpClient } from '@angular/common/http';
+import { serverURL } from '../../config'
+// import htttp client from angular
 
+//inject http into constructor
+// create function for SOS feature
+    // return this.http.post
+        // 1st param is url
+        // ngrok https
+        // 2nd param body
 @Injectable({
   providedIn: 'root'
 })
@@ -8,13 +17,20 @@ export class AddGuardianService {
  newGuardianName: string;
  newGuardianPhone: string;
 
+phoneNumber:`15044106484`;
+
+url: string = serverURL;
+endpoint: string = '/sms';
+
+sosMessage = `*Current User* has indicated they are experiencing an emergency. Please contact them and / or the authorities`;
+
 currentGuardians = [
     {name: 'Omar', phone: '504-444-4444'},
     {name: 'Chris', phone: '504-555-5555'},
     {name: 'Khari', phone: '504-555-5555'},
 ]
 
-  constructor(private extension: RouterExtensions) { }
+  constructor(private extension: RouterExtensions, public http: HttpClient) { }
 
     nameOnChange(input) {
         // console.log(input)
@@ -33,4 +49,19 @@ currentGuardians = [
         this.extension.backToPreviousPage();
     }
 
+    // create function for SOS feature
+    // return this.http.post
+        // 1st param is url
+        // ngrok https
+        // 2nd param body
+
+    // activateSOS() {
+    //     const { name, phone } = this.currentGuardians[0];
+    //     // const smsEndpoint = `/sms`;
+
+    //     // console.log(`${name}'s number is ${phone}. ${this.sosMessage}`)
+    //     console.log(`${this.url}${this.endpoint}`)
+    //     return this.http.get(`${this.url}${this.endpoint}`);
+
+    // }
 }
